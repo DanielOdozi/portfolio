0dbf26fca29711ce51a92a1ef53e696f327616b6
Daniel Odozi <odozidanielosy@gmail.com>
2023-10-02 18:04:03 +0100

    To be continued...

---
+++ src/Personal_project/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Test() {
+    return(
+        <div>
+            <ol>
+                <li>I am happy to relearn something i already know</li>
+            </ol>
+        </div>
+    )
+}
+
+export default Test
